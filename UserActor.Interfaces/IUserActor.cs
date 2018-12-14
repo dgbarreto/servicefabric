a31608b87147e1ab320efa10db02af627ed52a18
Danilo Barreto <dgbarreto@gmail.com>
2018-12-14 14:47:58 -0200

    Demo Actors

---
+++ UserActor.Interfaces/IUserActor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Remoting.FabricTransport;
+using Microsoft.ServiceFabric.Services.Remoting;
+
+namespace UserActor.Interfaces {
+    public interface IUserActor : IActor {
+        Task AddToBasket(Guid productID, int quantity);
+        Task<Dictionary<Guid, int>> GetBasket();
+        Task ClearBasket();
+    }
+}
---
+++ WebAPI/Controllers/BasketController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using UserActor.Interfaces;
+using WebAPI.Model;
+
+namespace WebAPI.Controllers {
+    [Route("api/basket")]
+    public class BasketController : Controller {
+        [HttpGet("get/{userId}")]
+        public async Task<ApiBasket> Get(string userId) {
+            IUserActor actor = GetActor(userId);
+
+            Dictionary<Guid, int> products = await actor.GetBasket();
+
+            return new ApiBasket() {
+                UserId = userId,
+                Items = products.Select(s => new ApiBasketItem {
+                    ProductId = s.Key.ToString(),
+                    Quantity = s.Value
+                }).ToArray()
+            };
+        }
+
+        [HttpPost("add/{userId}")]
+        public async Task Add(string userId, [FromBody] ApiBasketAddRequest request) {
+            IUserActor actor = GetActor(userId);
+
+            await actor.AddToBasket(request.ProductId, request.Quantity);
+        }
+
+        [HttpDelete("delete/{userId}")]
+        public async Task Delete(string userId) {
+            IUserActor actor = GetActor(userId);
+
+            await actor.ClearBasket();
+        }
+
+        private IUserActor GetActor(string userId) {
+            return ActorProxy.Create<IUserActor>(new ActorId(userId), new Uri("fabric:/ECommerce/UserActorService"));
+        }
+    }
+}
